89dbe90f8b1ea8ff3cc62439fe5e18a3a1492a4e
oloomoses <olooine@gmail.com>
2021-10-18 16:23:30 +0300

    Setup and install rubocop

--- school_library_decorator/rental.rb
+++ school_library_decorator/rental.rb
@@ -1,10 +1,12 @@
 class Rental
-  attr_accessor :date
-  attr_reader :person, :book
+  attr_accessor :date, :person, :book
 
   def initialize(date, person, book)
     @date = date
     @person = person
     @book = book
+
+    book.rentals << self
+    person.rentals << self
   end
 end
--- school_library_decorator/student.rb
+++ school_library_decorator/student.rb
@@ -1,9 +1,9 @@
-require './person.rb'
+require './person'
 
 class Student < Person
   attr_accessor :classroom
-  
-  def initialize(age, classroom, name = "Unknown", parent_permission = true)
+
+  def initialize(age, classroom, name = 'Unknown', parent_permission = true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
